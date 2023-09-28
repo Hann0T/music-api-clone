783a1a033dbb575a2ba546184b79d9ad631e6119
hans <hansolivas1@gmail.com>
2023-09-27 23:47:15 -0500

    refactor: model constructor only returns the model.

--- internal/models/album.go
+++ internal/models/album.go
@@ -1,12 +1,13 @@
 package models
 
 type Album struct {
+	ID       int    `json:"id"`
 	Title    string `json:"title"`
 	UPC      string `json:"upc"`
 	Cover    string `json:"cover"`
 	ArtistID int    `json:"artist_id"`
 }
 
-func NewAlbum(title string, upc string, cover string, artistID int) (Album, error) {
-	return Album{title, upc, cover, artistID}, nil
+func NewAlbum(id int, title string, upc string, cover string, artistID int) *Album {
+	return &Album{id, title, upc, cover, artistID}
 }
--- internal/models/artist.go
+++ internal/models/artist.go
@@ -1,11 +1,12 @@
 package models
 
 type Artist struct {
+	ID      int    `json:"id"`
 	Name    string `json:"name"`
 	Picture string `json:"picture"`
 	Fans    int    `json:"fans"`
 }
 
-func NewArtist(name string, picture string, fans int) (Artist, error) {
-	return Artist{name, picture, fans}, nil
+func NewArtist(id int, name string, picture string, fans int) *Artist {
+	return &Artist{id, name, picture, fans}
 }
