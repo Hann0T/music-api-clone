7a6b431d015b7e1a7f26aa8bb3015bc48b8b3f41
hans <hansolivas1@gmail.com>
2023-09-27 23:48:23 -0500

    feat: http go server.

--- internal/handler/http.go
+++ internal/handler/http.go
@@ -1,28 +1,76 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/Hann0/music-api-clone/internal/ports"
+	"github.com/go-chi/chi/v5"
 )
 
-func Login(w http.ResponseWriter, r *http.Request) {
+type Server struct {
+	AlbumRepository  ports.AlbumRepository
+	ArtistRepository ports.ArtistRepository
 }
 
-func Register(w http.ResponseWriter, r *http.Request) {
+func NewServer(albumRepository ports.AlbumRepository, artistRepository ports.ArtistRepository) *Server {
+	return &Server{
+		AlbumRepository:  albumRepository,
+		ArtistRepository: artistRepository,
+	}
+}
+
+func (s *Server) Login(w http.ResponseWriter, r *http.Request) {
+}
+
+func (s *Server) Register(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func Logout(w http.ResponseWriter, r *http.Request) {
+func (s *Server) Logout(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func GetAlbum(w http.ResponseWriter, r *http.Request) {
+func (s *Server) GetAlbum(w http.ResponseWriter, r *http.Request) {
+	albumID := chi.URLParam(r, "id")
+
+	if albumID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	id, err := strconv.Atoi(albumID)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	album, err := s.AlbumRepository.ReadAlbum(id)
+
+	if err != nil {
+        w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	resp, err := json.Marshal(album)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(resp)
+}
+
+func (s *Server) GetAlbums(w http.ResponseWriter, r *http.Request) {
+}
+
+func (s *Server) GetArtist(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func GetArtist(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func GetTrack(w http.ResponseWriter, r *http.Request) {
+func (s *Server) GetTrack(w http.ResponseWriter, r *http.Request) {
 
 }
