41fcde80ee1a659420ae884e00edac6edfeedd2e
hans <hansolivas1@gmail.com>
2023-09-27 23:48:44 -0500

    refactor: routes from server.

--- cmd/main.go
+++ cmd/main.go
@@ -4,24 +4,31 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Hann0/music-api-clone/internal/adapters/repositories"
 	"github.com/Hann0/music-api-clone/internal/handler"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	server := handler.NewServer(
+		repositories.NewInMemoryAlbumRepository(),
+		repositories.NewInMemoryArtistRepository(),
+	)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	// auth
-	r.Post("/login", handler.Login)
-	r.Post("/register", handler.Login)
-	r.Post("/logout", handler.Login)
+	r.Post("/login", server.Login)
+	r.Post("/register", server.Register)
+	r.Post("/logout", server.Logout)
 
 	// api
-	r.Get("/album/{id}", handler.GetAlbum)
-	r.Get("/artist/{id}", handler.GetArtist)
-	r.Get("/track/{id}", handler.GetTrack)
+	r.Get("/album/{id}", server.GetAlbum)
+	r.Get("/album", server.GetAlbums)
+	r.Get("/artist/{id}", server.GetArtist)
+	r.Get("/track/{id}", server.GetTrack)
 
-    log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
