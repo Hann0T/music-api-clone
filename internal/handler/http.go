129a815cd3bf44b21eec0b13f017c05a66d4f343
hans <hansolivas1@gmail.com>
2023-10-02 16:03:38 -0500

    refactor: using the DTOs.

--- internal/handler/http.go
+++ internal/handler/http.go
@@ -2,10 +2,10 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 
+	"github.com/Hann0/music-api-clone/internal/payloads"
 	"github.com/Hann0/music-api-clone/internal/ports"
 	"github.com/go-chi/chi/v5"
 )
@@ -39,29 +39,50 @@ func (s *Server) GetAlbum(w http.ResponseWriter, r *http.Request) {
 	albumID := chi.URLParam(r, "id")
 
 	if albumID == "" {
-		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
 	}
 
 	id, err := strconv.Atoi(albumID)
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, "Invalid ID", http.StatusInternalServerError)
 		return
 	}
 
 	album, err := s.AlbumRepository.ReadAlbum(id)
 
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
+		http.Error(w, "Not album found", http.StatusNotFound)
 		return
 	}
 
-	resp, err := json.Marshal(album)
+	artist, err := s.ArtistRepository.ReadArtist(album.ArtistID)
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Println("Error trying to marshal the model, error: ", err)
+		http.Error(w, "Not artist found", http.StatusNotFound)
+		return
+	}
+
+	tracks, err := s.TrackRepository.ReadTracksByAlbum(album)
+
+	if err != nil {
+		http.Error(w, "Not tracks found", http.StatusNotFound)
+		return
+	}
+
+	albumDTO := payloads.NewAlbumDTO(album)
+	albumDTO.SetArtist(payloads.NewArtistDTO(artist))
+	albumDTO.SetNumberTracks(len(tracks))
+
+	for _, track := range tracks {
+		albumDTO.AddTrack(payloads.NewTrackDTO(track))
+	}
+
+	resp, err := json.Marshal(albumDTO)
+
+	if err != nil {
+		http.Error(w, "Error marshalling data", http.StatusInternalServerError)
 		return
 	}
 
@@ -72,20 +93,25 @@ func (s *Server) GetAlbums(w http.ResponseWriter, r *http.Request) {
 	albums, err := s.AlbumRepository.ReadAlbums()
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, "Error fetching the albums", http.StatusNotFound)
 		return
 	}
 
 	if len(albums) <= 0 {
-		w.WriteHeader(http.StatusNotFound)
+		http.Error(w, "Not albums found", http.StatusNotFound)
 		return
 	}
 
-	resp, err := json.Marshal(albums)
+	var albumsDTO []*payloads.AlbumDTO
+
+	for _, album := range albums {
+		albumsDTO = append(albumsDTO, payloads.NewAlbumDTO(album))
+	}
+
+	resp, err := json.Marshal(albumsDTO)
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Println("Error trying to marshal the model, error: ", err)
+		http.Error(w, "Error marshalling data", http.StatusInternalServerError)
 		return
 	}
 
@@ -96,29 +122,37 @@ func (s *Server) GetArtist(w http.ResponseWriter, r *http.Request) {
 	artistID := chi.URLParam(r, "id")
 
 	if artistID == "" {
-		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
 	}
 
 	id, err := strconv.Atoi(artistID)
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, "Invalid ID", http.StatusInternalServerError)
 		return
 	}
 
-	album, err := s.ArtistRepository.ReadArtist(id)
+	artist, err := s.ArtistRepository.ReadArtist(id)
 
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
+		http.Error(w, "Not artist found", http.StatusNotFound)
 		return
 	}
 
-	resp, err := json.Marshal(album)
+	albums, err := s.AlbumRepository.ReadAlbumsByArtist(artist)
+	if err != nil {
+		http.Error(w, "Not albums found", http.StatusNotFound)
+		return
+	}
+
+	artistDTO := payloads.NewArtistDTO(artist)
+	artistDTO.SetNumberAlbums(len(albums))
+
+	resp, err := json.Marshal(artistDTO)
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Println("Error trying to marshal the model, error: ", err)
+		http.Error(w, "Error marshalling data", http.StatusInternalServerError)
 		return
 	}
 
@@ -126,33 +160,49 @@ func (s *Server) GetArtist(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) GetTrack(w http.ResponseWriter, r *http.Request) {
-	trackID := chi.URLParam(r, "id")
+	trackId := chi.URLParam(r, "id")
 
-	if len(trackID) <= 0 {
-		w.WriteHeader(http.StatusBadRequest)
+	if trackId == "" {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
 	}
 
-	id, err := strconv.Atoi(trackID)
+	id, err := strconv.Atoi(trackId)
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, "Invalid ID", http.StatusInternalServerError)
 		return
 	}
 
 	track, err := s.TrackRepository.ReadTrack(id)
 
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Println("Error trying to get the model, error: ", err)
+		http.Error(w, "No track found", http.StatusNotFound)
 		return
 	}
 
-	data, err := json.Marshal(track)
+	album, err := s.AlbumRepository.ReadAlbum(track.AlbumID)
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Println("Error trying to marshal the model, error: ", err)
+		http.Error(w, "No album found", http.StatusNotFound)
+		return
+	}
+
+	artist, err := s.ArtistRepository.ReadArtist(album.ArtistID)
+
+	if err != nil {
+		http.Error(w, "No artist found", http.StatusNotFound)
+		return
+	}
+
+	trackDTO := payloads.NewTrackDTO(track)
+	trackDTO.SetAlbum(payloads.NewAlbumDTO(album))
+	trackDTO.SetArtist(payloads.NewArtistDTO(artist))
+
+	data, err := json.Marshal(trackDTO)
+
+	if err != nil {
+		http.Error(w, "Error marshalling data", http.StatusInternalServerError)
 		return
 	}
 
