ccefa8988e9b274f8b81f7b15e0e88697ac71e17
hans <hansolivas1@gmail.com>
2023-10-10 22:50:22 -0500

    refactor: Using the postgres repository.

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/Hann0/music-api-clone/internal/adapters/repositories"
 	"github.com/Hann0/music-api-clone/internal/handler"
+	"github.com/Hann0/music-api-clone/migrations"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	connectionStr := "user=username dbname=deezer_go password=password sslmode=disable" // get data from env
+	db, err := sqlx.Connect("postgres", connectionStr)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = migrations.Run(connectionStr)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = migrations.PopulateDB(db)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	server := handler.NewServer(
-		repositories.NewInMemoryAlbumRepository(),
-		repositories.NewInMemoryArtistRepository(),
-		repositories.NewInMemoryTrackRepository(),
+		repositories.NewAlbumPostgresRepository(db),
+		repositories.NewArtistPostgresRepository(db),
+		repositories.NewTrackPostgresRepository(db),
 	)
 
 	r := chi.NewRouter()
@@ -32,5 +54,6 @@ func main() {
 	r.Get("/artist/{id}", server.GetArtist)
 	r.Get("/track/{id}", server.GetTrack)
 
+	fmt.Println("Server running in port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
