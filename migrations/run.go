769d7f101920a073c1d3c31fa797a3f77f46d69b
hans <hansolivas1@gmail.com>
2023-10-10 22:50:04 -0500

    refactor: new database data was added.

--- migrations/run.go
+++ migrations/run.go
@@ -50,5 +50,12 @@ func PopulateDB(db *sqlx.DB) error {
 	tx.MustExec("INSERT INTO albums (title, upc, cover, artist_id) VALUES ($1, $2, $3, $4)", "Korn", "463123", "https://dvdhaven.s3.amazonaws.com/3078/8718469536375.jpg", 1)
 	tx.MustExec("INSERT INTO albums (title, upc, cover, artist_id) VALUES ($1, $2, $3, $4)", "Life is peachy", "985135", "https://i.pinimg.com/originals/5c/5f/3e/5c5f3e1989415d7d5c10cf5bd0938344.jpg", 1)
 	tx.MustExec("INSERT INTO albums (title, upc, cover, artist_id) VALUES ($1, $2, $3, $4)", "Follow the leader", "233561", "http://www.metalmusicarchives.com/images/covers/korn-follow-the-leader-20141121221611.jpg", 1)
+
+	tx.MustExec("INSERT INTO tracks (title, duration, rank, album_id) VALUES ($1, $2, $3, $4)", "Take Me In Your Army", 254, 12, 1)
+	tx.MustExec("INSERT INTO tracks (title, duration, rank, album_id) VALUES ($1, $2, $3, $4)", "Human sadness", 1054, 5, 1)
+	tx.MustExec("INSERT INTO tracks (title, duration, rank, album_id) VALUES ($1, $2, $3, $4)", "QYURRYUS", 314, 4, 2)
+	tx.MustExec("INSERT INTO tracks (title, duration, rank, album_id) VALUES ($1, $2, $3, $4)", "Blind", 223, 1, 3)
+	tx.MustExec("INSERT INTO tracks (title, duration, rank, album_id) VALUES ($1, $2, $3, $4)", "Twist", 131, 1, 4)
+	tx.MustExec("INSERT INTO tracks (title, duration, rank, album_id) VALUES ($1, $2, $3, $4)", "Freak on a leash", 236, 1, 5)
 	return tx.Commit()
 }
